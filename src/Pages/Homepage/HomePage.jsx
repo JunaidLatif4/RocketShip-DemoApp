d39b59fb159de39e557c4df969ceabf105d291c8
JunaidLatif <junaidlatif004@gmail.com>
2022-11-27 19:36:30 +0500

    App Initieated

---
+++ src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./HomePage.scss"
+
+const HomePage = () => {
+  return (
+    <>
+    <div className="homepage_container">
+    This is HomePage
+    </div>
+    </>
+  )
+}
+
+export default HomePage
