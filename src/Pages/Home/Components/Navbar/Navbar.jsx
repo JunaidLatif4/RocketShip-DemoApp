9173133846428d80df9dc5ef01aeed2af7692a4e
shahzaibali01 <alishahzaib012345@gmail.com>
2022-11-28 13:35:00 +0500

    header done

--- src/Pages/Home/Components/Navbar/Navbar.jsx
+++ src/Pages/Home/Components/Navbar/Navbar.jsx
@@ -1,21 +1,36 @@
 import React from "react";
+
+// Pics :
+
 import logo from "../../../../Assets/logo.png";
-import "./Navbar.scss";
-import { AiOutlineArrowRight } from "react-icons/ai";
+
+// MUI :
+
 import Box from "@mui/material/Box";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import { AiOutlineMenu } from "react-icons/ai";
-import { AiOutlineClose } from "react-icons/ai";
+
 import Drawer from "@mui/material/Drawer";
+
 import MenuIcon from "@mui/icons-material/Menu";
+
 //  Recat icons :
+
+import { AiOutlineClose } from "react-icons/ai";
+
+import { AiOutlineArrowRight } from "react-icons/ai";
+
 import { AiOutlineTwitter } from "react-icons/ai";
 
 import { AiOutlineInstagram } from "react-icons/ai";
 
 import { AiFillFacebook } from "react-icons/ai";
 
+// CSS :
+
+import "./Navbar.scss";
+
 const Navbar = () => {
+  // Drawer Function or state
+
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -36,29 +51,45 @@ const Navbar = () => {
   };
 
   const list = (anchor: Anchor) => (
-    
     <Box
       sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
     >
       <>
         <div className="ham_nav_main">
-          <AiOutlineClose
-            style={{ color: "black" }}
-            className="icons"
-            onClick={toggleDrawer(anchor, false)}
-          />
-
           <div className="navbar__container">
+            <div className="nav_left">
+              <div className="logo">
+                <img src={logo} />
+                <div className="title">
+                  Rocket<span>Ship</span>
+                </div>
+              </div>
+              <AiOutlineClose
+                style={{ color: "white" }}
+                className="icons"
+                onClick={toggleDrawer(anchor, false)}
+              />
+            </div>
             <div className="nav_right">
-           
-            <div className="nav_text">Sign In</div>
-             
-              <div className="nav_text">Create An Account</div>
-              <button className="rent_btn">Rent Or Sell Your Place</button>
-              <div className="language">
-                <AiOutlineArrowRight />
-                Login
+              <div className="social_btn">
+                <AiOutlineTwitter />
+                <AiFillFacebook />
+                <AiOutlineInstagram />
+              </div>
+
+              <div className="options">
+                <div className="text">FAQs</div>
+                <div className="text">Rates</div>
+                <div className="text">Contact</div>
+                <div className="text" style={{ color: "#ff8d72" }}>
+                  Sign Up
+                </div>
+
+                <div className="log">
+                  Login
+                  <AiOutlineArrowRight className="arrow" />
+                </div>
               </div>
             </div>
           </div>
@@ -72,56 +103,48 @@ const Navbar = () => {
       <div className="nav_content">
         <div className="nav_left">
           <img src={logo} />
-          <div className="nav_logo_title">Rocket<span>Ship</span></div>
+          <div className="title">
+            Rocket<span>Ship</span>
+          </div>
         </div>
         <div className="nav_right">
-
           <div className="social_btn">
-          <AiFillFacebook/>
-          <AiOutlineInstagram/>
-          <AiOutlineTwitter/>
-          
+            <AiOutlineTwitter />
+            <AiFillFacebook />
+            <AiOutlineInstagram />
           </div>
 
-          <div className="nav_right_signin_options">
-      
-
-            <div className="nav_text">FAQs</div>
-            <div className="nav_text">Rates</div>
-            <div className="nav_text">Contact</div>
-            <div className="nav_text">Sign Up</div>
-     
-          
-            <div className="log">
-            Login
-              <AiOutlineArrowRight />
+          <div className="options">
+            <div className="text">FAQs</div>
+            <div className="text">Rates</div>
+            <div className="text">Contact</div>
+            <div className="text" style={{ color: "#ff8d72" }}>
+              Sign Up
             </div>
 
-          
+            <div className="log">
+              Login
+              <AiOutlineArrowRight className="arrow" />
+            </div>
           </div>
           <div className="ham_burger">
-              {["top"].map((anchor) => (
-                <React.Fragment key={anchor}>
-                  <MenuIcon
-                    style={{ color: "black" }}
-                    onClick={toggleDrawer(anchor, true)}
-                  />
-                  <Drawer
-                    anchor={anchor}
-                    open={state[anchor]}
-                    onClose={toggleDrawer(anchor, false)}
-                  >
-                    {list(anchor)}
-                  </Drawer>
-                </React.Fragment>
-              ))}
-            </div>
+            {["top"].map((anchor) => (
+              <React.Fragment key={anchor}>
+                <MenuIcon
+                  style={{ color: "white" }}
+                  onClick={toggleDrawer(anchor, true)}
+                />
+                <Drawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                >
+                  {list(anchor)}
+                </Drawer>
+              </React.Fragment>
+            ))}
+          </div>
         </div>
-
-
-
-
-
       </div>
     </div>
   );
